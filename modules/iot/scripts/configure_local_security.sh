797c893bcb72d49170c12d64f23e942247b29912
Frederic Descamps <lefred.descamps@gmail.com>
2022-03-08 21:40:01 +0100

    set crypto policies
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- modules/iot/scripts/configure_local_security.sh
+++ modules/iot/scripts/configure_local_security.sh
@@ -9,4 +9,7 @@ firewall-cmd --zone=public --permanent --add-port=1883/tcp
 firewall-cmd --zone=public --permanent --add-port=8883/tcp
 firewall-cmd --reload
 
+/usr/bin/update-crypto-policies --set LEGACY
+
 echo "Local Security Granted !"
+echo "Crypto Policies changed, reboot might be required !"
